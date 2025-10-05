fb39d99325ba8de865a3cee7af8acf0cb309bc4b
Anderson Luís Furlan <alsfurlan@gmail.com>
2025-10-05 12:17:15 -0300

    Resposta letra a

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { ThemeProvider, useTheme } from "./context/ThemeContext";
 function HomeScreen() {
   const { mode, toggleTheme } = useTheme();
   const isDark = mode === "dark";
+  const theme = styles.getTheme(isDark);
 
   return (
-    <View style={[styles.container]}>
-      <Text style={styles.text}>
+    <View
+      style={[styles.container, { backgroundColor: theme.backgroundColor }]}
+    >
+      <Text style={{ color: theme.color }}>
         Tema atual: {mode.toUpperCase()}
       </Text>
       <Button title="Alternar Tema" onPress={toggleTheme} />
@@ -30,13 +33,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    color: 'red'
-  },
   getTheme(isDark) {
     return {
       backgroundColor: isDark ? "#000" : "#fff",
       color: isDark ? "#fff" : "#000",
     };
-  }
+  },
 });
